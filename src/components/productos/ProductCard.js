d71af0d763f11df53059112ca9b21281a0c1f7ef
1727003855 <ejc.damian@yavirac.edu.ec>
2023-02-03 22:16:16 -0500

    fix bugs stock

--- src/components/productos/ProductCard.js
+++ src/components/productos/ProductCard.js
@@ -2,14 +2,14 @@ import {BsFillCartFill, BsFillHeartFill, BsSearch} from "react-icons/bs";
 
 export default function ProductCard({products, handleFormModal,handleFastBuy, title, addWishes}){
 
+    const productsOut = products.filter(value => value.stock <=0)
+    const productsAvailable = products.filter(value => value.stock >0)
 
     return(
         <div>
             <p className='text-azul ml-10 mt-2 font-titan'>{title}</p>
             <div className="flex flex-col justify-center grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-                {products?.stock > 0
-                    ?
-                    (products?.map((value, index)=>{
+                {productsAvailable?.map((value, index)=>{
                         return (
                             <div key={index} className="m-10">
                                 <div
@@ -49,35 +49,37 @@ export default function ProductCard({products, handleFormModal,handleFastBuy, ti
                                 </div>
                             </div>
                         );
-                    }))
-                    :(products?.map((value, index)=>{
-                        return (
-                            <div key={index} className="m-10">
-                                <div
-                                    className="bg-red-400 shadow hover:shadow-lg transition duration-300 ease-in-out xl:mb-0 lg:mb-0 md:mb-0 mb-6 cursor-no-drop group">
-                                    <h1 className="absolute text-5xl text-white rotate-45 text-6xl mt-16">Agotado</h1>
-                                    <div className="overflow-hidden relative h-80">
-                                        <img className="w-full h-full transition duration-700 ease-in-out group-hover:opacity-60 cursor-no-drop"
-                                             src={`http://127.0.0.1:8000${value.image}`}
-                                             alt=""/>
-                                        <div className="flex justify-center">
-                                            <div
-                                                className="absolute bottom-2 transition duration-500 ease-in-out opacity-0 group-hover:opacity-100">
+                    })
+                }
+                {
+                    productsOut?.map((value,index)=>{
+                                return (
+                                    <div key={index} className="m-10">
+                                        <div
+                                            className="bg-red-400 shadow hover:shadow-lg transition duration-300 ease-in-out xl:mb-0 lg:mb-0 md:mb-0 mb-6 cursor-no-drop group">
+                                            <h1 className="absolute text-5xl text-white rotate-45 text-6xl mt-16">Agotado</h1>
+                                            <div className="overflow-hidden relative h-80">
+                                                <img className="w-full h-full transition duration-700 ease-in-out group-hover:opacity-60 cursor-no-drop"
+                                                     src={`http://127.0.0.1:8000${value.image}`}
+                                                     alt=""/>
+                                                <div className="flex justify-center">
+                                                    <div
+                                                        className="absolute bottom-2 transition duration-500 ease-in-out opacity-0 group-hover:opacity-100">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="px-4 py-3 bg-white">
+                                                <h1 className="text-gray-800 font-semibold text-lg hover:text-red-500 transition duration-300 ease-in-out">
+                                                    {value.name}</h1>
+                                                <div className="flex py-2">
+                                                    <p className="mr-2 text-xs text-gray-600">USD ${value.priceDiscount}</p>
+                                                    <p className="mr-2 text-xs text-red-600 line-through">USD ${value.price}</p>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                    <div className="px-4 py-3 bg-white">
-                                        <h1 className="text-gray-800 font-semibold text-lg hover:text-red-500 transition duration-300 ease-in-out">
-                                            {value.name}</h1>
-                                        <div className="flex py-2">
-                                            <p className="mr-2 text-xs text-gray-600">USD ${value.priceDiscount}</p>
-                                            <p className="mr-2 text-xs text-red-600 line-through">USD ${value.price}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        );
-                    }))
+                        )
+                    })
                 }
             </div>
         </div>
